7430b528c1c3b7d460f67854ee8acf3e3424ea2d
Deivide Silva <37459501+DeivideSilva@users.noreply.github.com>
2020-10-30 16:35:11 -0300

    projeto finalizado

---
+++ flutter_chat/lib/main.dart
@@ -0,0 +1,30 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+void main() async{
+  runApp(MyApp());
+
+   /*Firestore.instance.collection('mensagens').snapshots().listen((dado) {
+     dado.documents.forEach((d) {
+       print(d.data);
+     });
+   });*/
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color:Colors.blue,
+        ),
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
