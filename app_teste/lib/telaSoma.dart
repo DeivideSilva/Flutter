f267ef697257a40235e4354e2842f9476301d908
Deivide Silva <37459501+DeivideSilva@users.noreply.github.com>
2020-12-08 18:26:59 -0300

    adicionado o botão de limpar campos

--- app_teste/lib/telaSoma.dart
+++ app_teste/lib/telaSoma.dart
@@ -9,12 +9,27 @@ class _TelaSomaState extends State<TelaSoma> {
   TextEditingController txtv1 = new TextEditingController();
   TextEditingController txtv2 = new TextEditingController();
   int v1, v2, total = 0;
+  final FocusNode myFocusNode = FocusNode();
 
-  void CalcularTotal() {
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  void calcularTotal() {
     setState(() {
       v1 = int.parse(txtv1.text);
       v2 = int.parse(txtv2.text);
       total = v1 + v2;
+    });
+  }
+
+  void limpaCampos() {
+    setState(() {
+      txtv1.text = '';
+      txtv2.text = '';
+      total = 0;
     });
   }
 
@@ -30,40 +45,54 @@ class _TelaSomaState extends State<TelaSoma> {
         child: Column(
           children: [
             TextField(
+              focusNode: myFocusNode,
               controller: txtv1,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
                   labelText: "Informe um número", enabled: true),
             ),
-
             TextField(
               controller: txtv2,
               keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: "Informe outro número"),
             ),
-
+            Divider(),
             Text(
               'O total é: $total ',
               style: TextStyle(fontSize: 25),
             ),
-            
-             RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              ),
-              onPressed: CalcularTotal,
-              color: Colors.blue,
-              child: Text(
-                "Calcular",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  onPressed: calcularTotal,
+                  color: Colors.blue,
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  onPressed: () {
+                    limpaCampos();
+                    FocusScope.of(context).requestFocus(myFocusNode);
+                  },
+                  color: Colors.blue,
+                  child: Text(
+                    "Limpar",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ],
             ),
-             
           ],
         ),
-       
-       
-        
       ),
     );
   }
